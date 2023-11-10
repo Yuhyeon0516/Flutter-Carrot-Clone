b993d32f497f1c999d647745d35fb9b0381815ff
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-10 20:28:28 +0900

    Created floating action button.

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,15 +1,58 @@
+import 'package:fast_app_base/screen/main/fab/w_floating_carrot_button.riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeFragment extends StatefulWidget {
+class HomeFragment extends ConsumerStatefulWidget {
   const HomeFragment({super.key});
 
   @override
-  State<HomeFragment> createState() => _HomeFragmentState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeFragmentState();
 }
 
-class _HomeFragmentState extends State<HomeFragment> {
+class _HomeFragmentState extends ConsumerState<HomeFragment> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      final floatingState = ref.read(floatingButtonStateProvider);
+
+      if (scrollController.position.pixels > 100 && !floatingState.isSmall) {
+        ref.read(floatingButtonStateProvider.notifier).changeButtonSize(true);
+      } else if (scrollController.position.pixels < 100 &&
+          floatingState.isSmall) {
+        ref.read(floatingButtonStateProvider.notifier).changeButtonSize(false);
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      controller: scrollController,
+      children: [
+        Container(
+          height: 500,
+          color: Colors.red,
+        ),
+        Container(
+          height: 500,
+          color: Colors.blue,
+        ),
+        Container(
+          height: 500,
+          color: Colors.red,
+        ),
+        Container(
+          height: 500,
+          color: Colors.blue,
+        ),
+        Container(
+          height: 500,
+          color: Colors.red,
+        ),
+      ],
+    );
   }
 }
