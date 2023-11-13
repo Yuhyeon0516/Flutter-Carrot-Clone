05078ca599e16a44082d1b0c6346ed332831c445
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-13 17:42:55 +0900

    Created write screen.

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,7 +1,7 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/entity/dummies.dart';
 import 'package:fast_app_base/screen/main/fab/w_floating_carrot_button.dart';
 import 'package:fast_app_base/screen/main/fab/w_floating_carrot_button.riverpod.dart';
+import 'package:fast_app_base/screen/main/tab/home/provider/post_provider.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_product_post_item.dart';
 import 'package:fast_app_base/screen/notification/s_notification.dart';
 import 'package:flutter/material.dart';
@@ -36,6 +36,7 @@ class _HomeFragmentState extends ConsumerState<HomeFragment> {
 
   @override
   Widget build(BuildContext context) {
+    final postList = ref.watch(postProvider);
     return Material(
       child: Column(
         children: [
--- lib/screen/post_detail/s_post_detail.dart
+++ lib/screen/post_detail/s_post_detail.dart
@@ -27,7 +27,7 @@ class PostDetailScreen extends ConsumerWidget {
 
     return productPost.when(
       data: (data) => _PostDetail(
-        data.simpleProductPost,
+        simpleProductPost ?? data.simpleProductPost,
         productPost: data,
       ),
       error: (error, trace) => "에러발생".text.make(),
@@ -165,18 +165,19 @@ class _ImagePager extends StatelessWidget {
                     ))
                 .toList(),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SmoothPageIndicator(
-                controller: pageController, // PageController
-                count: simpleProductPost.product.images.length,
-                effect: const JumpingDotEffect(
-                  verticalOffset: 10,
-                  dotColor: Colors.white54,
-                  activeDotColor: Colors.black45,
-                ), // your preferred effect
-                onDotClicked: (index) {}),
-          )
+          if (simpleProductPost.product.images.length > 1)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SmoothPageIndicator(
+                  controller: pageController, // PageController
+                  count: simpleProductPost.product.images.length,
+                  effect: const JumpingDotEffect(
+                    verticalOffset: 10,
+                    dotColor: Colors.white54,
+                    activeDotColor: Colors.black45,
+                  ), // your preferred effect
+                  onDotClicked: (index) {}),
+            )
         ],
       ),
     );
